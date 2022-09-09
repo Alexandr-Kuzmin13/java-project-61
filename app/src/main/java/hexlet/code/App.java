51e7665209cada517c4f1ad9d19210489540c3e9
Alexandr-Kuzmin13 <rusglagol@gmail.com>
2022-09-09 16:18:34 +0300

    Step 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,11 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Cli.greeting();
     }
 
 }
