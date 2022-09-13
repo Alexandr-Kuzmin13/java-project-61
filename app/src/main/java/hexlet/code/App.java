b2784867ee1526ed661e43768ce89f086c6037d6
Alexandr-Kuzmin13 <rusglagol@gmail.com>
2022-09-13 20:31:56 +0300

    Step 5

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,27 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        Cli.greeting();
+        Scanner number = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.\n"
+                + "1 - Greet\n"
+                + "2 - Even\n"
+                + "0 - Exit");
+        System.out.print("Your choice: ");
+        var first = number.nextInt();
+        System.out.println("");
+        switch (first) {
+            case 1:
+                Greet.greeting();
+                break;
+            case 2:
+                Even.gameEven();
+                break;
+        }
+
     }
 
 }
