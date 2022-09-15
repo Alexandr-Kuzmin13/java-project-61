7dbde74ee228734312eaefd3c62228346c798810
Alexandr-Kuzmin13 <rusglagol@gmail.com>
2022-09-15 18:37:31 +0300

    correction 9

---
+++ src/main/java/hexlet/code/App.java
@@ -0,0 +1,33 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
+        System.out.println("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                2 - Even
+                3 - Calc
+                4 - GCD
+                5 - Progression
+                6 - Prime
+                0 - Exit""");
+        System.out.print("Your choice: ");
+        var first = number.nextInt();
+        System.out.println(" ");
+        switch (first) {
+            case 1 -> Greet.greeting();
+            case 2 -> Even.gameEven();
+            // 3 - номер игры;
+            case 3 -> Calc.gameCalc();
+            case 4 -> GCD.gameGCD();
+            case 5 -> Progression.gameProgression();
+            case 6 -> Prime.gamePrime();
+        }
+
+    }
+
+}
