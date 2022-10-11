4672ed0efb6d48c9ac2a12bbb346f5d8931e177d
Alexandr-Kuzmin13 <rusglagol@gmail.com>
2022-10-11 10:08:09 +0300

    correction 43

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public final class Calc implements Game {
     private static final int OPERATOR_TYPE = 3;
-    private static final int SIZE_RANDOM1 = 10;
-    private static final int SIZE_RANDOM2 = 100;
+    private static final int NUMBER_RANGE_ONE = 10;
+    private static final int NUMBER_RANGE_TWO = 100;
     private static final String[] OPERATORS = {"+", "-", "*"};
 
     @Override
@@ -18,17 +18,16 @@ public final class Calc implements Game {
         String[] values = new String[2];
 
         Random rnd = new Random();
-        var firstNumber = rnd.nextInt(SIZE_RANDOM1);
-        var secondNumber = rnd.nextInt(SIZE_RANDOM2);
+        var firstNumber = rnd.nextInt(NUMBER_RANGE_ONE);
+        var secondNumber = rnd.nextInt(NUMBER_RANGE_TWO);
         String operator = OPERATORS[rnd.nextInt(OPERATOR_TYPE)];
         values[0] = firstNumber + " " + operator + " " + secondNumber;
 
-        if (operator.equals("+")) {
-            values[1] = String.valueOf(firstNumber + secondNumber);
-        } else if (operator.equals("-")) {
-            values[1] = String.valueOf(firstNumber - secondNumber);
-        } else {
-            values[1] = String.valueOf(firstNumber * secondNumber);
+        switch (operator) {
+            case "+" -> values[1] = String.valueOf(firstNumber + secondNumber);
+            case "-" -> values[1] = String.valueOf(firstNumber - secondNumber);
+            case "*" -> values[1] = String.valueOf(firstNumber * secondNumber);
+            default -> throw new RuntimeException("Unknown operator: " + operator);
         }
 
         return values;
